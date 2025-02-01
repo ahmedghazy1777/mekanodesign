8f787da6610cb294b819e47ee2d2087cc6ca5724
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2025-02-01 22:43:20 +0530

    fix3

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,13 +1,11 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
-export const dynamic = "force-dynamic"; // Ensure the route is treated as dynamic
+export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
-  const secret = request.nextUrl.searchParams.get(
-    "MekanoDesign@3X-?:KSfMNT+@)o%56"
-  );
-  if (secret !== "mekanodesign") {
+  const secret = request.nextUrl.searchParams.get("secret");
+  if (secret !== "MekanoDesign@3X-?:KSfMNT+@)o%56") {
     return NextResponse.json({ error: "Invalid secret" }, { status: 401 });
   }
   revalidatePath("/");
