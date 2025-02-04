a0ad91c242146459e259d4e8bf50eef2fd901b4f
pradeepkundekar0101 <pradeepkundekargithub@gmail.com>
2025-02-04 21:42:30 +0530

    feat: Integrate Testimonials with WordPress data and add Edge runtime support

--- src/app/api/revalidate/route.ts
+++ src/app/api/revalidate/route.ts
@@ -1,11 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { revalidatePath } from "next/cache";
 
+export const runtime = "edge"; // Add this line
 export const dynamic = "force-dynamic";
 
 export async function GET(request: NextRequest) {
   const secret = request.nextUrl.searchParams.get("secret");
-  if (secret !== "MekanoDesign@3X-?:KSfMNT+@)o%56") {
+  if (secret !== process.env.SECRET_KEY) {
     return NextResponse.json({ error: "Invalid secret" }, { status: 401 });
   }
   revalidatePath("/");
